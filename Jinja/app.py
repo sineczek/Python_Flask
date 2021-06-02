21adf2a56b96a4ba11686d2ee5f2b43ebc6b828d
Michal Zaitz <michalzaitz@gmail.com>
2021-06-02 19:14:00 +0200

    Dzień #156 - flash ...

--- Jinja/app.py
+++ Jinja/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
+from datetime import datetime
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'CosTajnegoNaGitHubaNiePrzewidzianego' #często jako zmienna środowiskowa zaczytywana, ew. z bazy danych
 
 # przechowywanie zmiennych walut
 class Currency:
@@ -19,6 +21,7 @@ class CantorOffer:
 
     def __init__(self):
         self.currencies = []
+        self.denied_codes = []
 
     def load_offer(self):
         self.currencies.append(Currency('USD', 'Dollar', 'united-states.png'))
@@ -26,6 +29,7 @@ class CantorOffer:
         self.currencies.append(Currency('JPY', 'Yen', 'japan.png'))
         self.currencies.append(Currency('GBP', 'Pound', 'united-kingdom.png'))
         self.currencies.append(Currency('PLN', 'Polski Złoty', 'poland.png'))
+        self.denied_codes.append('USD') #czyli komunikat tej waluty nie przyjmujemy
 
     def get_by_code(self, code):
         for currency in self.currencies:
@@ -71,9 +75,18 @@ def exchange():
         return render_template('exchange.html', offer=offer)
 
     else:
+        # flash("Debug: starting exchange in POST mode") # debug info na www
+        print("Debug: starting exchange in POST mode") #debug info w consoli
         currency = 'EUR'  # request.form['currency']
         if 'currency' in request.form:
             currency = request.form['currency']
+
+        if currency in offer.denied_codes:
+            flash('The currency {} cannot be accepted'.format(currency))
+        elif offer.get_by_code('unknown') == 'unknown':
+            flash('The selected currency is unknown and cannot be accepted')
+        else:
+            flash('Requested to exchange {} was accepted'.format(currency))
 
         amount = 100  # request.form['amount']
         if 'amount' in request.form:
@@ -99,33 +112,19 @@ def hotel_form():
 
         priority_type = notification_priorities.get_priority_by_code(priority)
         print('found', priority_type.code)
+
+        flash('Notification has been sent')
+
+        the_hour = datetime.now().hour
+        raise_priority = (the_hour >= 19 or the_hour < 6) and priority == 'medium'
+
+        if raise_priority:
+            priority = 'high'
+            flash('Rising priority from medium to high')
+
         return render_template('complain.html',
                                room_number=room_number, guest_name=guest_name,
                                notification_text=notification_text, priority_type=priority_type)
 
 
-"""          CIACH
 
-        room_number = 'None'  # request.form['room_number']
-        if 'room_number' in request.form:
-            room_number = request.form['room_number']
-
-        guest_name = 'None'  # request.form['guest_name']
-        if 'guest_name' in request.form:
-            guest_name = request.form['guest_name']
-
-        complain = 'None'  # request.form['complain']
-        if 'complain' in request.form:
-            complain = request.form['complain']
-        priority = request.form['priority']
-        if priority == 'High':
-            priority == 'Critical notification content'
-        elif priority == 'Medium':
-            priority == 'Important notification content'
-        else:
-            priority == 'Notification content'
-        return render_template('complain.html', room_number=room_number, guest_name=guest_name, complain=complain,
-                               priority=priority)
-                               
-                               
-              CIACH                 """

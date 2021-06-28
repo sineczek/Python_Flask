5eb05700e73a2a0a6199540b2bed993fe302d828
sinq <sineczek@gmail.com>
2021-06-28 21:57:16 +0200

    Dzien 181 debugging and fixing

--- NicerApp/app.py
+++ NicerApp/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, url_for, request, flash, g, redirect
+from flask import Flask, render_template, url_for, request, flash, g, redirect, session
 from datetime import datetime, date
 import sqlite3
+import random  # do hasła
+import string  # do operacji na napisach
+import hashlib  # do hashy haseł
+import binascii  # binarki na ascii
 
+# osr with password ujI
 # konfiguracja sqlite3
 app_info = {'db_file': '/Users/sinq/nauka/Python_Flask/NicerApp/data/cantor.db'}
 
@@ -17,10 +22,9 @@ def get_db():
         g.sqlite_db = conn
     return g.sqlite_db
 
-
 @app.teardown_appcontext
 def close_db(error):
-    if hasattr(g, 'sqlite3_db'):
+    if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
 
@@ -56,6 +60,50 @@ class CantorOffer:
         return Currency('unknown', 'unknown', 'pirate-flag.png')
 
 
+class UserPass:
+    def __init__(self, user='', password=''):
+        self.user = user
+        self.password = password
+
+    def hash_password(self):
+        """Hash a password for storing."""
+        # the value generated using os.urandom(60)
+        os_urandom_static = b"ID_\x12p:\x8d\xe7&\xcb\xf0=H1\xc1\x16\xac\xe5BX\xd7\xd6j\xe3i\x11\xbe\xaa\x05\xccc\xc2\xe8K\xcf\xf1\xac\x9bFy(\xfbn.`\xe9\xcd\xdd'\xdf`~vm\xae\xf2\x93WD\x04"
+        salt = hashlib.sha256(os_urandom_static).hexdigest().encode('ascii')
+        pwdhash = hashlib.pbkdf2_hmac('sha512', self.password.encode('utf-8'), salt, 100000)
+        pwdhash = binascii.hexlify(pwdhash)
+        return (salt + pwdhash).decode('ascii')
+
+    def verify_password(self, stored_password, provided_password):
+        """Verify a stored password against one provided by user"""
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'), salt.encode('ascii'), 100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        return pwdhash == stored_password
+
+    def get_random_user_pasword(self):
+        random_user = ''.join(random.choice(string.ascii_lowercase) for i in range(3))
+        self.user = random_user
+
+        password_characters = string.ascii_letters  # + string.digits + string.punctuation
+        random_password = ''.join(random.choice(password_characters) for i in range(3))
+        self.password = random_password
+
+    def login_user(self):
+        db = get_db()
+        sql_statement = 'select id, name, email, password, is_active, is_admin from users where name=?'
+        cur = db.execute(sql_statement, [self.user])
+        user_record = cur.fetchone()
+
+        if user_record != None and self.verify_password(user_record['password'], self.password):
+            return user_record
+        else:
+            self.user = None
+            self.password = None
+            return None
+
+
 class PriorityType:
     def __init__(self, code, description, selected):
         self.code = code
@@ -83,6 +131,57 @@ class NotificationPriorities:
 @app.route('/')
 def index():
     return render_template('index.html', active_menu='home')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.html', active_menu='login')
+    else:
+        user_name = '' if 'user_name' not in request.form else request.form['user_name']
+        user_pass = '' if 'user_pass' not in request.form else request.form['user_pass']
+
+        login = UserPass(user_name, user_pass)
+        login_record = login.login_user()
+
+        if login_record != None:
+            session['user'] = user_name
+            flash('Logon succesfull, welcome {}'.format(user_name))
+            return redirect(url_for('index'))
+        else:
+            flash('Logon failed, try again')
+            return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+
+    if 'user' in session:
+        session.pop('user', None)
+        flash('You are logged out')
+    return redirect(url_for('login'))
+
+@app.route('/init_app')
+def init_app():
+
+    # check if there are users defined (at least one active admin required)
+    db = get_db()
+    sql_statement = 'select count(*) as cnt from users where is_active and is_admin;'
+    cur = db.execute(sql_statement)
+    active_admins = cur.fetchone()
+
+    if active_admins!=None and active_admins['cnt']>0:
+        flash('Application is already set-up. Nothing to do')
+        return redirect(url_for('index'))
+
+    # if not - create/update admin account with a new password and admin privileges, display random username
+    user_pass = UserPass()
+    user_pass.get_random_user_pasword()
+    sql_statement = '''insert into users(name, email, password, is_active, is_admin)
+                       values(?,?,?,True, True);'''
+    db.execute(sql_statement, [user_pass.user, 'noone@nowhere.no', user_pass.hash_password()])
+    db.commit()
+    flash('User {} with password {} has been created'.format(user_pass.user, user_pass.password))
+    return redirect(url_for('index'))
 
 
 @app.route('/exchange', methods=['POST', 'GET'])

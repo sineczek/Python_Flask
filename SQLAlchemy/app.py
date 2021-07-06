b9b947eb10b4af27647a7fab0d62ef7c4a1b4d3c
sinq <sineczek@gmail.com>
2021-07-06 19:41:12 +0200

    Dzien 188 - sortowanie, zliczanie, ograniczanie, dodawanie i kasowanie danych w SQLAlchemy

--- SQLAlchemy/app.py
+++ SQLAlchemy/app.py
@@ -12,19 +12,33 @@ class Vendor(db.Model):
     discount = db.Column(db.Integer)
     active = db.Column(db.Boolean)
 
+    products = db.relationship('Product', backref='vendor', lazy='dynamic')
+
     def __repr__(self):
         return 'Vendor {}/{}'.format(self.id, self.name)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'))
+
+    def __repr__(self):
+        return 'Product {}/{}'.format(self.id, self.name)
+
 
 @app.route('/')
 def index():
     db.create_all()
-
-    #v1 = Vendor(id=1, name='Microsoft', discount=0, active=True)
-    #db.session.add(v1)
-    #db.session.commit()
-    #v2 = Vendor(id=2, name='Apple', discount=0, active=True)
-    #db.session.add(v2)
-    #db.session.commit()
+    a1 = Product(id=201, name='MacBook Air M1', vendor=2)
+    db.session.add(a1)
+    db.session.commit
+    # v1 = Vendor(id=1, name='Microsoft', discount=0, active=True)
+    # db.session.add(v1)
+    # db.session.commit()
+    # v2 = Vendor(id=2, name='Apple', discount=0, active=True)
+    # db.session.add(v2)
+    # db.session.commit()
 
     vendors = Vendor.query.all()
 

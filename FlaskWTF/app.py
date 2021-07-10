271b8fa738e5411fb25a095fce50f4aebe977c23
sinq <sineczek@gmail.com>
2021-07-10 22:35:00 +0200

    dzien 194 validatory w wtforms

--- FlaskWTF/app.py
+++ FlaskWTF/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import DataRequired, Length, ValidationError
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'co§n1eNaGiT4'
@@ -8,15 +9,24 @@ app.config['SECRET_KEY'] = 'co§n1eNaGiT4'
 
 class BookForm(FlaskForm):
 
-    title = StringField('Book title')
-    amount = IntegerField('Amount')
+    def validate_amount_even(form, field):
+        if field.data % 2 != 1:
+            raise ValidationError('This number must be even!')
+
+
+    title = StringField('Book title', validators=[DataRequired('Enter book title'),
+                                                  Length(min=5,
+                                                         max=50,
+                                                         message="The title must have 5 - 50 characters")])
+    amount = IntegerField('Amount', validators=[DataRequired('Enter amount'),
+                          validate_amount_even])
     available = BooleanField('Available')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    form = BookForm()
 
+    form = BookForm(csrf_enabled=True)
     if form.validate_on_submit():
         return f'''<h1>Hello</h1>
                     <ul>

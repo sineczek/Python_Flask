5a72ea6f61eadc7b3f1f71c0d95c5580781545a3
sinq <sineczek@gmail.com>
2021-07-14 18:30:07 +0200

    dzien 198 kontrolki HTML5

--- FlaskWTF/app.py
+++ FlaskWTF/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired, Length, ValidationError, Email
+from wtforms.fields.html5 import DateField, IntegerField
+import os
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from werkzeug.utils import secure_filename
+from datetime import date
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'co§n1eNaGiT4'
@@ -9,10 +14,12 @@ app.config['SECRET_KEY'] = 'co§n1eNaGiT4'
 
 
 class Book:
-    def __init__(self, title, amount, available):
+    def __init__(self, title, amount, available, email, offer_date):
         self.title = title
         self.amount = amount
         self.available = available
+        self.email = email
+        self.offer_date = offer_date
 
 
 class BookForm(FlaskForm):
@@ -31,15 +38,24 @@ class BookForm(FlaskForm):
                           validate_amount_even],
                           default=1)
     available = BooleanField('Available')
+    cover = FileField("Book cover", validators=[FileRequired(),
+                                                FileAllowed(['jpg', 'png'], "Sorry, we accept only png and jpg files")])
+    offer_date = DateField('Offer date')#, format='%Y-%m-%d')
+
+class BookFormEmail(BookForm):
+    email = StringField("e-mail", validators=[Email()])
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    book = Book(title='How to take a nice photo', amount=11, available=True)
-    #form = BookForm(title='European Kings', amount=1, available=True)
-    form = BookForm(obj=book)
+    book = Book(title='How to take a nice photo', amount=11, available=True, email="", offer_date=date.today())
+    form = BookFormEmail(obj=book)
 
     if form.validate_on_submit():
+
+        f = form.cover.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.root_path, 'static', 'covers', filename))
 
         form.populate_obj(book)
         return f'''<h1>Hello</h1>
@@ -47,6 +63,9 @@ def index():
                         <li>{form.title.label}: {book.title}</li>
                         <li>{form.amount.label}: {book.amount}</li>
                         <li>{form.available.label}: {book.available}</li>
+                        <li>email: {book.email}</li>
+                        <li>offer date: {book.offer_date}</li>
+                        <li><img src={url_for('static', filename='covers/{}'.format(filename))}</li>
                     </ul>'''
     return render_template('index.html', form=form)
 
